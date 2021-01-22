bed777e8904f1a6f220ed8fbefda0511365bd097
オレンジくん <kazup0689@gmail.com>
2021-01-22 23:11:14 +0900

    Add: コーディング用モックファイル

---
+++ app/controllers/mock/users_controller.rb
@@ -0,0 +1,7 @@
+class Mock::UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  namespace :mock do
+    get 'users/index'
+    get 'users/show'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
