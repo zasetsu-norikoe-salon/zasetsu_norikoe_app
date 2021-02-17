e521d4930452cf679c74a9b27df49ebc224bfb5d
saki10 <rsp10111011@gmail.com>
2021-02-17 22:38:43 +0900

    usersテーブルマイグレーション訂正　カンマ、削除日時、t.timestamps型訂正

--- db/migrate/20210207135604_users.rb
+++ db/migrate/20210207135604_users.rb
@@ -1,9 +1,9 @@
 class Users < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email, null:false comment:'メールアドレス'
-      t.string :password, null:false comment:'パスワード'
-      t.string :name, null:false comment:'slack名'
+      t.string :email, null:false,comment:'メールアドレス'
+      t.string :password, null:false, comment:'パスワード'
+      t.string :name, null:false,comment:'slack名'
       t.integer :gender, null:false, default:1, comment:'性別'
       t.string :age, comment:'世代'
       t.integer :employment_form, null:false, default:1, comment:'就業形態'
@@ -12,7 +12,7 @@ class Users < ActiveRecord::Migration[5.2]
       t.string :qualification, comment:'資格取得'
       t.string :hobby, comment:'趣味'
       t.string :special_skill, comment:'得意なこと'
-      t.string :want_talk, comment: '話したいこと'
+      t.string :want_talk, comment:'話したいこと'
       t.string :not_want_talk, comment:'話したくないこと'
       t.text :free_area, comment:'フリーエリア'
       t.integer :zasetsu_count, null: false, default:0, comment:'挫折カウント'
@@ -21,9 +21,8 @@ class Users < ActiveRecord::Migration[5.2]
       t.string :twitter_url, comment:'Twitter URL'
       t.string :github_url, comment:'GitHub URL'
       t.string :port_url, comment:'ポートフォリオURL'
-      t.timestamps :deleted_at, comment:'削除日時'
-      t.timestamps :created_at, comment:'作成日時'
-      t.timestamps :updated_at, comment:'更新日時'
-    end
+      t.datetime :deleted_at,comment:'削除日時'
+      t.timestamps 
+      t.timestamps 
   end
 end
