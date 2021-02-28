39763eec13a33a93c873b8f2ab50350dc27c7908
オレンジくん <kazup0689@gmail.com>
2021-02-28 11:39:04 +0900

    Fix: RuboCop自動修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
     sequence(:email) { Faker::Internet.free_email }
     sequence(:password) { 'password' }
-    sequence(:name) { Faker::Name.first_name}
+    sequence(:name) { Faker::Name.first_name }
     sequence(:gender) { Faker::Number.within(range: 1..4) }
     sequence(:employment_form) { Faker::Number.within(range: 1..5) }
     sequence(:zasetsu_count) { Faker::Number.digit }
