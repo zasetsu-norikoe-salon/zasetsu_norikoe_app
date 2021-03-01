1045a4435d015c3a205bd8a1d631044782dfa5b9
オレンジくん <kazup0689@gmail.com>
2021-03-02 00:08:17 +0900

    Fix: it -> example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,43 +6,45 @@ RSpec.describe User, type: :model do
   # FactoryBotでUserをオブジェクト化
   let(:user) { build(:user) }
 
-  it 'メールアドレス、パスワード、名前、性別、就業形態、挫折カウントが入力されている場合、有効である' do
-    expect(user).to be_valid
-  end
+  describe 'モデルバリデーション' do
+    example 'メールアドレス、パスワード、名前、性別、就業形態、挫折カウントが入力されている場合、有効である' do
+      expect(user).to be_valid
+    end
 
-  it 'メールアドレスが無い場合、無効である' do
-    user.email = ''
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
+    example 'メールアドレスが無い場合、無効である' do
+      user.email = ''
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
 
-  it 'パスワードが無い場合、無効である' do
-    user.password = ''
-    user.valid?
-    expect(user.errors[:password]).to include("can't be blank")
-  end
+    example 'パスワードが無い場合、無効である' do
+      user.password = ''
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
 
-  it '名前が無い場合、無効である' do
-    user.name = ''
-    user.valid?
-    expect(user.errors[:name]).to include("can't be blank")
-  end
+    example '名前が無い場合、無効である' do
+      user.name = ''
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
 
-  it '性別が無い場合、無効である' do
-    user.gender = ''
-    user.valid?
-    expect(user.errors[:gender]).to include("can't be blank")
-  end
+    example '性別が無い場合、無効である' do
+      user.gender = ''
+      user.valid?
+      expect(user.errors[:gender]).to include("can't be blank")
+    end
 
-  it '就業形態が無い場合、無効である' do
-    user.employment_form = ''
-    user.valid?
-    expect(user.errors[:employment_form]).to include("can't be blank")
-  end
+    example '就業形態が無い場合、無効である' do
+      user.employment_form = ''
+      user.valid?
+      expect(user.errors[:employment_form]).to include("can't be blank")
+    end
 
-  it '挫折カウントが無い場合、無効である' do
-    user.zasetsu_count = ''
-    user.valid?
-    expect(user.errors[:zasetsu_count]).to include("can't be blank")
+    example '挫折カウントが無い場合、無効である' do
+      user.zasetsu_count = ''
+      user.valid?
+      expect(user.errors[:zasetsu_count]).to include("can't be blank")
+    end
   end
 end
