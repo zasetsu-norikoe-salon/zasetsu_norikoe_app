ed90beca528a0c58ab0a55dd524ff4662ca3be44
オレンジくん <kazup0689@gmail.com>
2021-03-01 23:55:55 +0900

    Add: 中間テーブルのFactory

--- spec/factories/user_and_skill_category_relationships.rb
+++ spec/factories/user_and_skill_category_relationships.rb
@@ -2,5 +2,7 @@
 
 FactoryBot.define do
   factory :user_and_skill_category_relationship do
+    association(:user)
+    association(:skill_category)
   end
 end
--- spec/factories/user_and_skill_set_relationships.rb
+++ spec/factories/user_and_skill_set_relationships.rb
@@ -2,5 +2,8 @@
 
 FactoryBot.define do
   factory :user_and_skill_set_relationship do
+    association(:user)
+    association(:skill_set)
+    sequence(:level) { Faker::Number.within(range: 1..3) }
   end
 end
