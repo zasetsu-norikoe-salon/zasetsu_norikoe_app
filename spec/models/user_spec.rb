572a5a0788c71efda30a12c5bc6e9d0301c7608b
オレンジくん <kazup0689@gmail.com>
2021-03-03 10:23:07 +0900

    Fix: example -> it

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,41 +7,41 @@ RSpec.describe User, type: :model do
   let(:user) { build(:user) }
 
   describe 'モデルバリデーション' do
-    example 'メールアドレス、パスワード、名前、性別、就業形態、挫折カウントが入力されている場合、有効である' do
+    it 'メールアドレス、パスワード、名前、性別、就業形態、挫折カウントが入力されている場合、有効である' do
       expect(user).to be_valid
     end
 
-    example 'メールアドレスが無い場合、無効である' do
+    it 'メールアドレスが無い場合、無効である' do
       user.email = ''
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
 
-    example 'パスワードが無い場合、無効である' do
+    it 'パスワードが無い場合、無効である' do
       user.password = ''
       user.valid?
       expect(user.errors[:password]).to include("can't be blank")
     end
 
-    example '名前が無い場合、無効である' do
+    it '名前が無い場合、無効である' do
       user.name = ''
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
 
-    example '性別が無い場合、無効である' do
+    it '性別が無い場合、無効である' do
       user.gender = ''
       user.valid?
       expect(user.errors[:gender]).to include("can't be blank")
     end
 
-    example '就業形態が無い場合、無効である' do
+    it '就業形態が無い場合、無効である' do
       user.employment_form = ''
       user.valid?
       expect(user.errors[:employment_form]).to include("can't be blank")
     end
 
-    example '挫折カウントが無い場合、無効である' do
+    it '挫折カウントが無い場合、無効である' do
       user.zasetsu_count = ''
       user.valid?
       expect(user.errors[:zasetsu_count]).to include("can't be blank")
