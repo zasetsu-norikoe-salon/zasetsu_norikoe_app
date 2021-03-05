54a2d2592fafde63568a979ecd04f7c820e863d4
オレンジくん <kazup0689@gmail.com>
2021-03-06 00:13:55 +0900

    Update: UserのFactory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,5 +8,10 @@ FactoryBot.define do
     sequence(:gender) { %i[not_known male female other].sample }
     sequence(:employment_type) { %i[full_time diff_full_time side_biz freelance internship job_seeker].sample }
     sequence(:zasetsu_count) { Faker::Number.digit }
+
+    after(:create) do |user|
+      create(:user_and_skill_category_relationship, user: user)
+      create(:user_and_skill_set_relationship, user: user)
+    end
   end
 end
