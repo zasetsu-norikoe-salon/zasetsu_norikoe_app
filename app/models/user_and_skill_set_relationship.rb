ca862f430429a062578a20ef5ca1ca62fa42ba0e
オレンジくん <kazup0689@gmail.com>
2021-03-05 21:31:46 +0900

    Add: Enumerizeでuser_and_skill_set_relationshipのlevelを設定

--- app/models/user_and_skill_set_relationship.rb
+++ app/models/user_and_skill_set_relationship.rb
@@ -2,4 +2,8 @@
 
 class UserAndSkillSetRelationship < ApplicationRecord
   acts_as_paranoid
+  extend Enumerize
+
+  # 経験値(勉強中: 1, 実務経験あり: 2, ベテラン: 3)
+  enumerize :level, in: { junior: 1, middle: 2, veteran: 3 }, default: :junior
 end
