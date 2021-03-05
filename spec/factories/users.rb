d7f67004a14c150453bd4c295af9edb124223f78
オレンジくん <kazup0689@gmail.com>
2021-03-06 01:19:33 +0900

    Fix: 関連テーブルの作成をtraitで指定できるように

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,9 +9,18 @@ FactoryBot.define do
     sequence(:employment_type) { %i[full_time diff_full_time side_biz freelance internship job_seeker].sample }
     sequence(:zasetsu_count) { Faker::Number.digit }
 
-    after(:create) do |user|
-      create(:user_and_skill_category_relationship, user: user)
-      create(:user_and_skill_set_relationship, user: user)
+    # スキル属性も一緒に作成する
+    trait :with_skill_categories do
+      after(:create) do |user|
+        create(:user_and_skill_category_relationship, user: user)
+      end
+    end
+
+    # スキルセットも一緒に作成する
+    trait :with_skill_sets do
+      after(:create) do |user|
+        create(:user_and_skill_set_relationship, user: user)
+      end
     end
   end
 end
