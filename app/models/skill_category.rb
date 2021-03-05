491ae55b7cd6e3d65a584195da9ae3411e64a228
オレンジくん <kazup0689@gmail.com>
2021-03-06 00:13:22 +0900

    Add: 各モデルのアソシエーション

--- app/models/skill_category.rb
+++ app/models/skill_category.rb
@@ -2,4 +2,8 @@
 
 class SkillCategory < ApplicationRecord
   acts_as_paranoid
+
+  # Association
+  has_many :user_and_skill_category_relationships, dependent: :destroy
+  has_many :users, through: :user_and_skill_category_relationships
 end
--- app/models/user_and_skill_set_relationship.rb
+++ app/models/user_and_skill_set_relationship.rb
@@ -4,6 +4,10 @@ class UserAndSkillSetRelationship < ApplicationRecord
   acts_as_paranoid
   extend Enumerize
 
+  # Association
+  belongs_to :user
+  belongs_to :skill_set
+
   # 経験値(勉強中: 1, 実務経験あり: 2, ベテラン: 3)
   enumerize :level,
             in: { junior: 1, middle: 2, veteran: 3 },
