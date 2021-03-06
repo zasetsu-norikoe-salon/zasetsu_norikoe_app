827666a9cf35ad5027f458947beea61149a15d37
滝 和弥 <54909391+kazup1125@users.noreply.github.com>
2021-03-06 20:20:54 +0900

    UserAndSkillSetRelationshipのバリデーションとそのテスト (#50)
    Co-authored-by: オレンジくん <kazup0689@gmail.com>

--- spec/models/user_and_skill_set_relationship_spec.rb
+++ spec/models/user_and_skill_set_relationship_spec.rb
@@ -6,6 +6,18 @@ RSpec.describe UserAndSkillSetRelationship, type: :model do
   # FactoryBotでUserAndSkillSetRelationshipをオブジェクト化
   let(:user_and_skill_set_relationship) { build(:user_and_skill_set_relationship) }
 
+  describe 'モデルバリデーション' do
+    it 'user_id, skill_set_id, 経験値が入力されている場合、有効である' do
+      expect(user_and_skill_set_relationship).to be_valid
+    end
+
+    it '経験値が未入力の場合、無効である' do
+      user_and_skill_set_relationship.level = ''
+      user_and_skill_set_relationship.valid?
+      expect(user_and_skill_set_relationship.errors[:level]).to include(I18n.t('errors.messages.blank'))
+    end
+  end
+
   describe 'アソシーエション' do
     let(:association) do
       described_class.reflect_on_association(target)
