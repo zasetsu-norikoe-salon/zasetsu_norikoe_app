a660608f461319de38ac66c996dce4b9edd9c490
オレンジくん <kazup0689@gmail.com>
2021-03-06 11:24:47 +0900

    Add: SkillSetのアソシエーションのテスト

--- spec/models/skill_set_spec.rb
+++ spec/models/skill_set_spec.rb
@@ -3,5 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe SkillSet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # FactoryBotでSkillSetをオブジェクト化
+  let(:skill_set) { build(:skill_set) }
+
+  describe 'アソシーエション' do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    context 'Userモデルとの関係' do
+      let(:target) { :users}
+      it 'has_manyである' do
+        expect(association.macro).to eq :has_many
+      end
+    end
+
+    context 'UserAndSkillSetRelationshipモデルとの関係' do
+      let(:target) { :user_and_skill_set_relationships }
+      it 'has_manyである' do
+        expect(association.macro).to eq :has_many
+      end
+    end
+  end
 end
