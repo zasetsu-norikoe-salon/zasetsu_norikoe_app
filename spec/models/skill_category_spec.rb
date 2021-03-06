77e15b34dbbbb6f8b58e6634b63476c8b9a89224
オレンジくん <kazup0689@gmail.com>
2021-03-06 11:26:43 +0900

    Add: SkillCategoryのアソシエーションのテスト

--- spec/models/skill_category_spec.rb
+++ spec/models/skill_category_spec.rb
@@ -3,5 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe SkillCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # FactoryBotでSkillCategoryをオブジェクト化
+  let(:skill_category) { build(:skill_category) }
+
+  describe 'アソシーエション' do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    context 'Userモデルとの関係' do
+      let(:target) { :users}
+      it 'has_manyである' do
+        expect(association.macro).to eq :has_many
+      end
+    end
+
+    context 'UserAndSkillCategoryRelationshipモデルとの関係' do
+      let(:target) { :user_and_skill_category_relationships }
+      it 'has_manyである' do
+        expect(association.macro).to eq :has_many
+      end
+    end
+  end
 end
