30ab744acea2d8c788d2e565c82cef1abd40be15
オレンジくん <kazup0689@gmail.com>
2021-03-08 23:10:56 +0900

    Add: ページネーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.page(params[:page])
   end
 
   # GET /users/1 or /users/1.json
