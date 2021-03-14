1029e5414a6eceb7c3a23aad0558bca52b77c3eb
滝 和弥 <54909391+kazup1125@users.noreply.github.com>
2021-03-14 22:09:03 +0900

    本番環境にBasic認証をかける (#72)
    * Update: 本番環境のみBasic認証をかける
    
    * Add: コメント
    
    Co-authored-by: オレンジくん <kazup0689@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :request_path
+  before_action :basic_auth_only_production
 
   # 参考:https://qiita.com/toduq/items/a9fa48926b060b2d8a5b
   # @path.is('users#index')のように書けばコントローラー#アクションで条件分岐可能
@@ -13,5 +14,16 @@ class ApplicationController < ActionController::Base
       str.map { |s| include?(s) }.include?(true)
     end
     # rubocop:enable Lint/NestedMethodDefinition
+  end
+
+  private
+
+  # 本番環境にBasic認証をかける。ユーザー名とパスワードは環境変数で管理する
+  def basic_auth_only_production
+    return unless Rails.env.production?
+
+    authenticate_or_request_with_http_basic do |name, password|
+      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
   end
 end
